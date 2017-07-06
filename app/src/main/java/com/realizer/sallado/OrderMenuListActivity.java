2c9687601cb45b529aab80eeed86f51595b297f8
sachinAtRealizerTech <sachin.shinde@realizertech.com>
2017-07-06 16:40:29 +0530

    11 th commit

--- app/src/main/java/com/realizer/sallado/OrderMenuListActivity.java
+++ app/src/main/java/com/realizer/sallado/OrderMenuListActivity.java
@@ -186,6 +186,8 @@ public class OrderMenuListActivity extends AppCompatActivity {
                         placeName.add(deliveryPoint.getAddress());
                     }
 
+                    //test
+
                     if(placeName.size()>0) {
                         ArrayAdapter<String> adapter = new ArrayAdapter<String>(OrderMenuListActivity.this,
                                 android.R.layout.simple_spinner_item, placeName);
